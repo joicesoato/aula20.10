c8de4d97d6e97e024da2873803059b2bb6c95b71
Joice Soato <soatojoyce@gmail.com>
2025-11-04 01:44:57 +0000

    aula 03-11 pt2

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -15,6 +15,16 @@ public class Main {
         notSMS.definirPrioridade(8);
         System.out.println(notSMS.obterNivelPrioridade());
         notSMS.definirPrioridade(0);
+
+        Notificacao notif = new NotificacaoSms(
+            "Fulano", "1423456543", "Testando", 3);
+        notif.exibirInformacoesRemetente();
+        notif.enviar();
+
+        Priorizavel objPriorizavel = new NotificacaoSms(
+            "Ciclano", "134334567", "Novo Teste", 2);
+        
+        objPriorizavel.definirPrioridade(6);
+        
     }
 }
-
